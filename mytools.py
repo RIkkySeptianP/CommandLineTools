72b554162456c8c4bad6970486103942ea2daa33
RIkkySeptianP <rikky.septian.p@gmail.com>
2021-10-01 22:43:35 +0700

    CLI done

--- mytools.py
+++ mytools.py
@@ -1,4 +1,7 @@
 import argparse
+from pathlib import Path
+import json
+
 
 
 def runcommandtools():
@@ -8,21 +11,60 @@ def runcommandtools():
     parser.add_argument(
         'ffoldername',
         type=str,
-        help="Enter your folder addr"
+        help="Enter your log folder addr"
+    )
+
+    parser.add_argument(
+        '-t',
+        type=str,
+        help="Select your format file json or text, if none or not both of them will be plaintext"
+    )
+
+    parser.add_argument(
+        '-o',
+        type=str,
+        help="Select your output folder, if none output will be at"
     )
 
     args = parser.parse_args()
 
     filename = args.ffoldername
+    typefile = args.t
+    if typefile == "json" or typefile == "text":
+        pass
+    else:
+        typefile = "text"
 
-    print("your folder name is {0}".format(filename))
+    if typefile == "text":
+        fileext = "txt"
+    else:
+        fileext = "json"
+    outputfilename = args.o if args.o else "mytools.{0}".format(fileext)
+
+    print("your folder name is {0} and yor type {1}".format(filename, typefile))
+    fle = Path(outputfilename)
+    fle.touch(exist_ok=True)
     with open(filename) as file:
         lines = file.readlines()
-
         count = 0
+        data = {}
+        text = ""
+        data['data'] = []
         for line in lines:
             count += 1
-            print(f'line {count}: {line}')
+            if fileext == "json":
+                data['data'].append({
+                    'count': f'{count}',
+                    'text': f'{line}'
+                })
+            else:
+                text += f'{line}\n'
+        if fileext == "json":
+            with open(outputfilename, 'w') as outfile:
+                json.dump(data, outfile)
+        else:
+            with open(outputfilename, 'w') as the_file:
+                the_file.write(f'{text}\n')
 
 
 if __name__ == '__main__':
